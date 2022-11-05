4a0a7f50b88889f8efa550e673acc22487a85d48
ethanbrucee <ebruce@uab.edu>
2022-11-04 21:17:52 -0500

    navbar changes

--- client/src/pages/HomePage/components/Navbar/Navbar.jsx
+++ client/src/pages/HomePage/components/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import ReactLogo from 'assets/smartHomeIcon.png';
+import Routes from 'constants/routes';
+import { Link } from 'react-router-dom';
 import Colors from '../../../../constants/colors';
 
 const Container = styled.div`
@@ -8,6 +11,51 @@ const Container = styled.div`
     background-color: ${Colors.lightBlue};
 `;
 
-const Navbar = () => <Container />;
+const Nav = styled(Link)`
+    background: #000
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem calc((100vw-1000px)/2);
+    z-index: 10;
+`;
 
+const NavLink = styled.link`
+    color: #fff;
+    display: flex;
+    align-items:center;
+    text-decoration: none;
+    padding: 0 1rem;
+    height: 100%
+    cursor: pointer;
+`;
+
+const StyledNavbar = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: -24px;
+`;
+
+const HomeLogo = styled.img`
+    width: 50px;
+    height: 50px;
+    margin-top: -15px;
+`;
+
+const Navbar = () => (
+    <Container>
+        <Nav>
+            <NavLink to={Routes.Home}>
+                <HomeLogo className="HomeLogo" src={ReactLogo} />
+            </NavLink>
+            <StyledNavbar>
+                <NavLink to={Routes.FloorPlan}>Floor Plan</NavLink>
+                <NavLink to={Routes.Home}>Home</NavLink>
+                <NavLink to={Routes.Login}>Login/Logout</NavLink>
+                <NavLink to={Routes.Maintenance}>Maintenance</NavLink>
+                <NavLink to={Routes.UsageData}>Usage</NavLink>
+            </StyledNavbar>
+        </Nav>
+    </Container>
+);
 export default Navbar;
