96109d953d2de4259da1ff626523f3a921f01710
Carlos Rivas <rivascarlosva@gmail.com>
2022-11-04 21:46:23 -0500

    fix navbar

--- client/src/pages/HomePage/components/Navbar/Navbar.jsx
+++ client/src/pages/HomePage/components/Navbar/Navbar.jsx
@@ -6,56 +6,45 @@ import { Link } from 'react-router-dom';
 import Colors from '../../../../constants/colors';
 
 const Container = styled.div`
-    width: 100%;
-    min-height: 50px;
-    background-color: ${Colors.lightBlue};
-`;
-
-const Nav = styled(Link)`
-    background: #000
-    height: 80px;
     display: flex;
+    width: 100%;
+    min-height: 80px;
+    background-color: ${Colors.lightBlue};
     justify-content: space-between;
-    padding: 0.5rem calc((100vw-1000px)/2);
-    z-index: 10;
 `;
 
-const NavLink = styled.link`
+const NavLink = styled(Link)`
     color: #fff;
     display: flex;
-    align-items:center;
+    align-items: center;
     text-decoration: none;
-    padding: 0 1rem;
-    height: 100%
+    padding: 0 15px;
     cursor: pointer;
 `;
 
 const StyledNavbar = styled.div`
     display: flex;
     align-items: center;
-    margin-right: -24px;
+    margin: 0 15px;
+    gap: 10px;
 `;
 
 const HomeLogo = styled.img`
     width: 50px;
     height: 50px;
-    margin-top: -15px;
 `;
 
 const Navbar = () => (
     <Container>
-        <Nav>
-            <NavLink to={Routes.Home}>
-                <HomeLogo className="HomeLogo" src={ReactLogo} />
-            </NavLink>
-            <StyledNavbar>
-                <NavLink to={Routes.FloorPlan}>Floor Plan</NavLink>
-                <NavLink to={Routes.Home}>Home</NavLink>
-                <NavLink to={Routes.Login}>Login/Logout</NavLink>
-                <NavLink to={Routes.Maintenance}>Maintenance</NavLink>
-                <NavLink to={Routes.UsageData}>Usage</NavLink>
-            </StyledNavbar>
-        </Nav>
+        <NavLink to={Routes.Home}>
+            <HomeLogo className="HomeLogo" src={ReactLogo} />
+        </NavLink>
+        <StyledNavbar>
+            <NavLink to={Routes.FloorPlan}>Floor Plan</NavLink>
+            <NavLink to={Routes.Maintenance}>Maintenance</NavLink>
+            <NavLink to={Routes.UsageData}>Usage</NavLink>
+            <NavLink to={Routes.Login}>Logout</NavLink>
+        </StyledNavbar>
     </Container>
 );
 export default Navbar;
