241f79a700e0f3987a559fe3d4757e03a505dd06
chrisconl <chrisman1997@gmail.com>
2022-11-07 13:19:19 -0600

    Added device data csv and changed DB connections to close properly after use

--- server/api/api.py
+++ server/api/api.py
@@ -13,23 +13,28 @@ Port = 5432
 DB_Name = "Team1DB"
 DB_Host = "138.26.48.83"
 
-conn = psycopg2.connect(database=DB_Name, user=UserID, password=Password, host=DB_Host, port=Port)
-cur = conn.cursor()
+
 
 class login_validate(Resource): 
     def post(self):
         #Takes in a post request with login info in the form of a json and check it against the database to confirm there is a user that matches that username password pair and returns a json with the userId on a valid pair or a different json with an incorrect login message
         json = request.get_json()
+        conn = psycopg2.connect(database=DB_Name, user=UserID, password=Password, host=DB_Host, port=Port)
+        cur = conn.cursor()
         cur.execute(f"SELECT * FROM users WHERE email = '{json['email']}' AND password = '{json['password']}';")
         query = cur.fetchall()
         if len(query) == 1:
+            conn.close()
             return jsonify({"userId":query[0][0]})
         else:
+            conn.close
             return make_response(jsonify({"message":"Incorrect email or password"}), 401)
 
 class get_rooms(Resource):
     def get(self):
         #Queries the database and adds the data from each room into a dictionary which is then put into a list to create a json file to be sent to the frontend for rendering
+        conn = psycopg2.connect(database=DB_Name, user=UserID, password=Password, host=DB_Host, port=Port)
+        cur = conn.cursor()
         cur.execute(f"SELECT * FROM rooms;")
         rooms = cur.fetchall()
         rooms_list = []
@@ -43,6 +48,7 @@ class get_rooms(Resource):
             room_dict['height'] = room[5]
             room_dict['devices'] = []
             rooms_list.append(room_dict)
+        conn.close()
         return jsonify(rooms_list)
 
 api.add_resource(login_validate, '/login_validate', '/api/login')
