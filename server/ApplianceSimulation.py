d83a353575ef5ff93d326069dbd2d333861285cb
jemar <jemar_austin@hotmail.com>
2022-11-06 22:22:45 -0600

    Update ApplianceSimulation.py

--- server/ApplianceSimulation.py
+++ server/ApplianceSimulation.py
@@ -4,9 +4,9 @@
 from math import floor,ceil
 
 def electric(watts: int) -> float:
-    # Electricity is $0.12 per kWh (1w = 1/1000kw)"""
+    # Electricity is $0.12 per kWh = W X time (hours) / 1000kw)
     cost = (watts / 1000) * .12
-    print(f"The cost for bathroom fan is {cost} per kwh.")
+    print(f"*** Usage cost is {cost} kwh.")
     return cost
 
 def lightBulbs() -> float:
@@ -93,6 +93,53 @@ def ovenS_S() -> float:
     cost = (electric(watts) * min_day) * numberUnits
     return cost 
 
+def LivingRoomTvM_F() -> float:
+    #  Week Day Living Room TV uses 636 watts per hour at 240 minutes per day
+    watts = 636
+    min_day = 240 / 60
+    numberUnits = 1
+    cost = (electric(watts) * min_day) * numberUnits
+    return cost  
+
+def LivingRoomTvS_S() -> float:
+    #  Week End Living Room TV uses 636 watts per hour at 480 minutes per day
+    watts = 636
+    min_day = 480 / 60
+    numberUnits = 1
+    cost = (electric(watts) * min_day) * numberUnits
+    return cost 
+
+def BedroomTvM_F() -> float:
+    #  Week Day oven uses 636 watts per hour at 45 minutes per day
+    watts = 100
+    min_day = 120 / 60
+    numberUnits = 1
+    cost = (electric(watts) * min_day) * numberUnits
+    return cost  
+
+def BedroomTvS_S() -> float:
+    #  Week End oven uses 636 watts per hour at 60 minutes per day
+    watts = 100
+    min_day = 240 / 60
+    numberUnits = 1
+    cost = (electric(watts) * min_day) * numberUnits
+    return cost 
+
+"""
+def waterHeater(gallons_of_hot_water) -> float:
+    watts = 4500
+    min_to_heat = 4 / 60
+    cost = (electric(watts) / min_to_heat) * gallons_of_hot_water
+    return cost   
+
+def dishwasher() -> float:
+    watts = 1800
+    min_day = 11.25 / 60
+    gallons_of_hot_water = 6
+    cost = (electric(watts) / min_day) + waterHeater(gallons_of_hot_water)
+    return cost
+"""
+
 def temperatureDiffernce(internalTemp, externalTemp):
     #takes the internal and external temperature and return how large that difference is in intervals of +/-10 so a difference of 9 would result in 0 and 29 would result in 2 
     tempDiffernce = externalTemp - internalTemp
@@ -100,6 +147,8 @@ def temperatureDiffernce(internalTemp, externalTemp):
         return ceil(tempDiffernce/10) #rounds up in the case of negative numbers i.e. -9.5/10 since rounding down would go to -1 and rounding up would go to 0 as we want it to
     else:
         return floor(tempDiffernce/10)
+
+
 """
 def add(num1: int, num2: int) -> int:
     # Add two numbers
@@ -150,3 +199,15 @@ print(f"The cost for the oven M-F is {ans} per day.")
 
 ans = ovenS_S()
 print(f"The cost for the oven S-S is {ans} per day.")
+
+ans = LivingRoomTvM_F()
+print(f"The cost for the Living Room TV M-F is {ans} per day.")
+
+ans = LivingRoomTvS_S()
+print(f"The cost for the Livng Room TV S-S is {ans} per day.")
+
+ans = BedroomTvM_F ()
+print(f"The cost for the Bedroom TV M-F is {ans} per day.")
+
+ans = BedroomTvS_S ()
+print(f"The cost for the Bedroom TV S-S is {ans} per day.")
