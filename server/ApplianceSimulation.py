d25b58ce4078d11789f9756efdb0a10d5dd73b56
jemar <jemar_austin@hotmail.com>
2022-11-08 17:24:44 -0600

    Update ApplianceSimulation.py

--- server/ApplianceSimulation.py
+++ server/ApplianceSimulation.py
@@ -1,13 +1,32 @@
 # CS499
 # Team1
+# Last Modified 11-07-2022
 
 from math import floor,ceil
+
+""" Functions to calculate daily usage functions """
 
 def electric(watts: int) -> float:
     # Electricity is $0.12 per kWh = W X time (hours) / 1000kw)
     cost = (watts / 1000) * .12
     print(f"*** Usage cost is {cost} kwh.")
     return cost
+
+def water(gallons: int) -> float:
+    # Electricity is $2.52 per 748 gallons)
+    cost = (gallons / 748) * 2.52
+    print(f"*** Water cost is ${cost}.")
+    return cost
+
+def waterHeater(gallons_of_hot_water: int) -> float:
+    # Calculates hot water heater reheating at 4500 watts per hour at 4 minutes per day
+    watts = 4500
+    min_to_heat = 4 / 60
+    cost = (electric(watts) * min_to_heat) * gallons_of_hot_water
+    return cost   
+
+
+""" Daily usage functions to import data"""
 
 def lightBulbs() -> float:
     # Each bulbs uses 60 watts per hour
@@ -53,14 +72,6 @@ def microwaveS_S() -> float:
     cost = (electric(watts) * min_day) * numberUnits
     return cost       
 
-def waterHeater() -> float:
-    #  Daily hot water heater uses 4500 watts per hour at 4 minutes per day
-    watts = 4500
-    min_day = 4 / 60
-    numberUnits = 1
-    cost = (electric(watts) * min_day) * numberUnits
-    return cost     
-
 def stoveM_F() -> float:
     #  Week Day stove uses 3500 watts per hour at 15 minutes per day
     watts = 3500
@@ -93,7 +104,7 @@ def ovenS_S() -> float:
     cost = (electric(watts) * min_day) * numberUnits
     return cost 
 
-def LivingRoomTvM_F() -> float:
+def livingRoomTvM_F() -> float:
     #  Week Day Living Room TV uses 636 watts per hour at 240 minutes per day
     watts = 636
     min_day = 240 / 60
@@ -101,7 +112,7 @@ def LivingRoomTvM_F() -> float:
     cost = (electric(watts) * min_day) * numberUnits
     return cost  
 
-def LivingRoomTvS_S() -> float:
+def livingRoomTvS_S() -> float:
     #  Week End Living Room TV uses 636 watts per hour at 480 minutes per day
     watts = 636
     min_day = 480 / 60
@@ -109,7 +120,7 @@ def LivingRoomTvS_S() -> float:
     cost = (electric(watts) * min_day) * numberUnits
     return cost 
 
-def BedroomTvM_F() -> float:
+def bedroomTvM_F() -> float:
     #  Week Day oven uses 636 watts per hour at 45 minutes per day
     watts = 100
     min_day = 120 / 60
@@ -117,7 +128,7 @@ def BedroomTvM_F() -> float:
     cost = (electric(watts) * min_day) * numberUnits
     return cost  
 
-def BedroomTvS_S() -> float:
+def bedroomTvS_S() -> float:
     #  Week End oven uses 636 watts per hour at 60 minutes per day
     watts = 100
     min_day = 240 / 60
@@ -125,7 +136,80 @@ def BedroomTvS_S() -> float:
     cost = (electric(watts) * min_day) * numberUnits
     return cost 
 
+""" Daily usage functions to import data: Baths"""
+
+def bathsShowerWaterM_F() -> float:
+     #  Week Day shower uses 25 gallons per bath at 2 per day
+    gallons = 25
+    perDay = 2
+    cost = (water(gallons) * perDay) 
+    return cost
+
+def bathsBathWaterM_F() -> float:
+     #  Week Day bath uses 30 gallons per bath at 2 per day
+    gallons = 30
+    perDay = 2
+    cost = (water(gallons) * perDay) 
+    return cost
+
+def bathsShowerWaterS_S() -> float:
+     #  Week End shower uses 25 gallons per bath at 2 per day
+    gallons = 25
+    perDay = 3
+    cost = (water(gallons) * perDay) 
+    return cost
+
+def bathsBathWaterS_S() -> float:
+     #  Week End bath uses 30 gallons per bath at 2 per day
+    gallons = 30
+    perDay = 3
+    cost = (water(gallons) * perDay) 
+    return cost
+
+def bathsShowerElectricalM_F() -> float:
+     #  Week Day shower uses 25 gallons at 65% hot water per bath at 2 per day
+    gallons = 25
+    hotWaterPercent = 0.65
+    perDay = 2
+    cost = (waterHeater(gallons * hotWaterPercent) * perDay)    
+    return cost
+
+def bathsBathElectricalM_F() -> float:
+     #  Week Day shower uses 30 gallons at 65% hot water per bath at 2 per day
+    gallons = 30
+    hotWaterPercent = 0.65
+    perDay = 2
+    cost = (waterHeater(gallons * hotWaterPercent) * perDay)    
+    return cost
+
+def bathsShowerElectricalS_S() -> float:
+     #  Week Day shower uses 25 gallons at 65% hot water per bath at 2 per day
+    gallons = 25
+    hotWaterPercent = 0.65
+    perDay = 3
+    cost = (waterHeater(gallons * hotWaterPercent) * perDay)    
+    return cost
+
+def bathsBathElectricalS_S() -> float:
+     #  Week Day shower uses 30 gallons at 65% hot water per bath at 2 per day
+    gallons = 30
+    hotWaterPercent = 0.65
+    perDay = 3
+    cost = (waterHeater(gallons * hotWaterPercent) * perDay)    
+    return cost
+
+
+
+
 """
+def waterHeater() -> float:
+    #  Daily hot water heater uses 4500 watts per hour at 4 minutes per day
+    watts = 4500
+    min_day = 4 / 60
+    numberUnits = 1
+    cost = (electric(watts) * min_day) * numberUnits
+    return cost  
+
 def waterHeater(gallons_of_hot_water) -> float:
     watts = 4500
     min_to_heat = 4 / 60
@@ -149,20 +233,9 @@ def temperatureDiffernce(internalTemp, externalTemp):
         return floor(tempDiffernce/10)
 
 
-"""
-def add(num1: int, num2: int) -> int:
-    # Add two numbers
-    num3 = num1 + num2
-
-    return num3
-"""
 
 # Driver Code
-""""
-num1, num2 = 5, 15
-ans = add(num1, num2)
-print(f"The addition of {num1} and {num2} results {ans}.")
-"""
+
 watts = 3500
 ans = electric(watts)
 print(f"The cost for {watts} watts is {ans} per kwh.")
@@ -185,8 +258,8 @@ print(f"The cost for microwave M-F is {ans} per day.")
 ans = microwaveS_S()
 print(f"The cost for microwave S-S is {ans} per day.")
 
-ans = waterHeater()
-print(f"The cost for the hot water heater is {ans:.3f} per day.")
+# ans = waterHeater()
+# print(f"The cost for the hot water heater is {ans:.3f} per day.")
 
 ans = stoveM_F()
 print(f"The cost for the stove M-F is {ans} per day.")
@@ -200,14 +273,38 @@ print(f"The cost for the oven M-F is {ans} per day.")
 ans = ovenS_S()
 print(f"The cost for the oven S-S is {ans} per day.")
 
-ans = LivingRoomTvM_F()
+ans = livingRoomTvM_F()
 print(f"The cost for the Living Room TV M-F is {ans} per day.")
 
-ans = LivingRoomTvS_S()
+ans = livingRoomTvS_S()
 print(f"The cost for the Livng Room TV S-S is {ans} per day.")
 
-ans = BedroomTvM_F ()
-print(f"The cost for the Bedroom TV M-F is {ans} per day.")
+ans = bedroomTvM_F ()
+print(f"The electrical cost for the Bedroom TV M-F is {ans} per day.")
 
-ans = BedroomTvS_S ()
-print(f"The cost for the Bedroom TV S-S is {ans} per day.")
+ans = bedroomTvS_S ()
+print(f"The electrical cost for the Bedroom TV S-S is {ans} per day.")
+
+ans = bathsShowerWaterM_F ()
+print(f"The water cost for the Showers M-F is {ans} per day.")
+
+ans = bathsBathWaterM_F ()
+print(f"The water cost for the Baths M-F is {ans} per day.")
+
+ans = bathsShowerWaterS_S ()
+print(f"The water cost for the Showers S-S is {ans} per day.")
+
+ans = bathsBathWaterS_S ()
+print(f"The water cost for the Baths S-S is {ans} per day.")
+
+ans = bathsShowerElectricalM_F ()
+print(f"The electrical cost for hot water for the Showers M-F is {ans} per day.")
+
+ans = bathsBathElectricalM_F ()
+print(f"The electrical cost for hot water for the Bath M-F is {ans} per day.")
+
+ans = bathsShowerElectricalS_S ()
+print(f"The electrical cost for hot water for the Showers S-S is {ans} per day.")
+
+ans = bathsBathElectricalS_S ()
+print(f"The electrical cost for hot water for the Bath S-S is {ans} per day.")
